d771521536f6727fe8e73d69b666a996afa07609
Martin Henry <martinhenry@unknownb8e8560b02a0.att.net>
2014-07-16 21:41:53 -0700

    incoming body prints

--- gothai.go
+++ gothai.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+  "encoding/json"
   "fmt"
   "log"
   "net/http"
   "os"
+  "io/ioutil"
 )
+
+type Message struct {
+  Word string
+}
 
 func main() {
   http.HandleFunc("/submitWord", handler)
@@ -22,7 +28,12 @@ func handler(w http.ResponseWriter, r *http.Request) {
   w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
   w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token")
   w.Header().Set("Access-Control-Allow-Credentials", "true")
-  fmt.Println(r)
+
+  body, err := ioutil.ReadAll(r.Body)
+  var msg Message
+  err = json.Unmarshal(body, &msg)
+  _ = err
+  fmt.Println(msg)
   fmt.Fprintf(w, "Hello this is our first Go web program!")
 }
 
