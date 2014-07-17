151cfbab4a1b170506ccfbd79a654b8438448bd3
Martin Henry <martinhenry@unknownb8e8560b02a0.att.net>
2014-07-16 22:10:41 -0700

    handle first question

--- gothai.go
+++ gothai.go
@@ -12,9 +12,13 @@ import (
 type Message struct {
   Word string
 }
+type Question struct {
+  Number int `json:"questionNumber"`
+}
 
 func main() {
   http.HandleFunc("/submitWord", handler)
+  http.HandleFunc("/getQuestion", getQuestionHandler)
   fmt.Println("Listening...")
   err := http.ListenAndServe(GetPort(),nil)
   if err != nil {
@@ -32,6 +36,7 @@ func handler(w http.ResponseWriter, r *http.Request) {
   body, err := ioutil.ReadAll(r.Body)
   var msg Message
   err = json.Unmarshal(body, &msg)
+
   _ = err
   fmt.Println(msg)
   fmt.Fprintf(w, "Hello this is our first Go web program!")
@@ -46,3 +51,21 @@ func GetPort() string {
   return ":" + port
 }
 
+func getQuestionHandler(w http.ResponseWriter, r *http.Request) {
+  w.Header().Set("Access-Control-Allow-Origin", "*")
+  w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+  w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token")
+  w.Header().Set("Access-Control-Allow-Credentials", "true")
+
+  body, err := ioutil.ReadAll(r.Body)
+  var questionNum Question
+  err = json.Unmarshal(body, &questionNum)
+
+  _ = err
+  switch questionNum.Number {
+  case 0:
+    fmt.Fprintf(w, "What's your name?")
+  case 1:
+    fmt.Fprintf(w, "How old are you?")
+  }
+}
