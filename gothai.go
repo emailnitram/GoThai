9bf756d08c250d9905623f2f921bc3c814f96384
Martin Henry <emailnitram@gmail.com>
2014-08-10 15:40:49 -0700

    read from mongodb and serve up simple json response

--- gothai.go
+++ gothai.go
@@ -3,86 +3,57 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+	"time"
+	// "io/ioutil"
 	"log"
 	"net/http"
-	"os"
+	// "os"
 )
 
-type Message struct {
-	Word string
-}
 type Question struct {
-	Number int `json:"questionNumber"`
+	Id            int
+	Name          string
+	Answers       []string
+	correctAnswer int
 }
-type Response map[string]interface{}
 
-func (r Response) String() (s string) {
-	b, err := json.Marshal(r)
-	if err != nil {
-		s = ""
-		return
-	}
-	s = string(b)
-	return
-}
+var currentQuestionNum int = 0
 
 func main() {
-	http.HandleFunc("/submitWord", handler)
-	http.HandleFunc("/getQuestion", getQuestionHandler)
-	fmt.Println("Listening...")
-	err := http.ListenAndServe(GetPort(), nil)
+
+	// err = c.Insert(&Question{0, "What is the capital of Thailand?", []string{"Phuket", "Bangkok", "Chiang Mai", "Krabi"}, 1},
+	// 	&Question{1, "What is currency used in Thailand?", []string{"Dollar", "Dong", "Baht", "Yen"}, 2})
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	http.HandleFunc("/question", getQuestion)
+	log.Fatal(http.ListenAndServe(":4747", nil))
+}
+
+func getQuestion(w http.ResponseWriter, r *http.Request) {
+	maxWait := time.Duration(5 * time.Second)
+	session, err := mgo.DialWithTimeout("localhost:27017", maxWait)
 	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
-		return
+		panic(err)
 	}
-}
+	defer session.Close()
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token")
-	w.Header().Set("Access-Control-Allow-Credentials", "true")
-
-	body, err := ioutil.ReadAll(r.Body)
-	var msg Message
-	err = json.Unmarshal(body, &msg)
-
-	_ = err
-	fmt.Println(msg)
-
-	if msg.Word == "Martin" {
-		fmt.Fprint(w, Response{"success": true})
-	} else {
-		fmt.Fprint(w, Response{"success": false})
+	// Optional. Switch the session to a monotonic behavior.
+	session.SetMode(mgo.Monotonic, true)
+	c := session.DB("thaiQuiz").C("questions")
+	result := Question{}
+	err = c.Find(bson.M{"id": currentQuestionNum}).One(&result)
+	if err != nil {
+		panic(err)
 	}
-
-}
-
-func GetPort() string {
-	var port = os.Getenv("PORT")
-	if port == "" {
-		port = "4747"
-		fmt.Println("INFO: No Port environment variable detected, defaulting to " + port)
+	fmt.Println(result)
+	b, err := json.Marshal(result)
+	if err != nil {
+		panic(err)
 	}
-	return ":" + port
-}
-
-func getQuestionHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token")
-	w.Header().Set("Access-Control-Allow-Credentials", "true")
-
-	body, err := ioutil.ReadAll(r.Body)
-	var questionNum Question
-	err = json.Unmarshal(body, &questionNum)
-
-	_ = err
-	switch questionNum.Number {
-	case 0:
-		fmt.Fprintf(w, "What's your name?")
-	case 1:
-		fmt.Fprintf(w, "How old are you?")
-	}
+	w.Write(b)
+	// currentQuestionNum++
 }
