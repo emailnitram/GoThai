a0301038c2151d2f82d5215bb2d0849980aeedf0
Martin Henry <emailnitram@gmail.com>
2014-08-13 21:21:54 -0700

    upadted Gulpfile

---
+++ Gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp'),
+    gutil = require('gulp-util'),
+    jshint = require('gulp-jshint'),
+    browserify = require('gulp-browserify'),
+    concat = require('gulp-concat'),
+    clean = require('gulp-clean');
+
+// JSHint task
+gulp.task('lint', function() {
+  gulp.src('./app/scripts/*.js')
+  .pipe(jshint())
+  .pipe(jshint.reporter('default'));
+});
+
+// Browserify task
+gulp.task('browserify', function() {
+  // Single point of entry (make sure not to src ALL your files, broweserify will figure it out for you)
+  gulp.src(['app/scripts/main.js'])
+  .pipe(browserify({
+    insertGlobals: true,
+    debug: true
+  }))
+  // Bundle to a single file
+  .pipe(concat('bundle.js'))
+  // Output it to our dist folder
+  .pipe(gulp.dest('dist/js'));
+});
+
+gulp.task('watch', ['lint'], function() {
+  // Watch our scripts
+  gulp.watch(['app/scripts/*.js', 'app/scripts/**/*.js], [
+    'lint',
+    'broswerify'
+    ]);
+});
+
